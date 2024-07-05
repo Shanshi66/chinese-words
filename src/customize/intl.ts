13ef31520bfef976669ccf1f68d029dae42880e6
luosen <heyrosen@163.com>
2024-07-05 09:47:12 +0800

    word页面多语言

--- src/customize/intl.ts
+++ src/customize/intl.ts
@@ -22,51 +22,76 @@ interface ListIntlData {
     description: string;
 }
 
+interface WordIntlData {
+    information: string;
+    explanation: string;
+    writing: string;
+    relatedList: string;
+    homophones: string;
+}
+
 export interface IntlData {
     header: HeaderIntlData;
     home: HomeIntlData;
     lists: ListIntlData;
+    word: WordIntlData;
 }
 
 export const englishIntlData: IntlData = {
-    "header": {
-        "lists": "Lists",
-        "home": "Home",
+    header: {
+        lists: "Lists",
+        home: "Home",
     },
-    "home": {
-        "heroTitle": "Find And Learn Chinese Words",
-        "searchPlaceholder": "Search Chinese words by pinyin or characters",
-        "wordListTitle": "Chinese Word List",
-        "factsTitle": "How many words included?",
-        "factsWordCount": "WORDS",
-        "factsListCount": "LISTS",
-        "factsExampleCount": "EXAMPLES",
-        "faqTitle": "Frequently Asked Questions",
+    home: {
+        heroTitle: "Find And Learn Chinese Words",
+        searchPlaceholder: "Search Chinese words by pinyin or characters",
+        wordListTitle: "Chinese Word List",
+        factsTitle: "How many words included?",
+        factsWordCount: "WORDS",
+        factsListCount: "LISTS",
+        factsExampleCount: "EXAMPLES",
+        faqTitle: "Frequently Asked Questions",
     },
-    "lists": {
-        "title": "Chinese Word List",
-        "description": "Collect Chinese words in different scenarios and purposes, and organize them into lists for targeted learning",
+    lists: {
+        title: "Chinese Word List",
+        description: "Collect Chinese words in different scenarios and purposes, and organize them into lists for targeted learning",
+    },
+    word: {
+        information: "Information",
+        explanation: "Explanation",
+        writing: "Writing",
+        relatedList: "Related List",
+        homophones: "Homophones",
     }
+
 }
 
 export const chineseIntlData: IntlData = {
-    "header": {
-        "lists": "列表",
-        "home": "首页"
+    header: {
+        lists: "列表",
+        home: "首页"
     },
-    "home": {
-        "heroTitle": "查找并学习中文词",
-        "searchPlaceholder": "通过拼音或汉字搜索中文词",
-        "wordListTitle": "中文词分类表",
-        "factsTitle": "网站收录数量",
-        "factsWordCount": "词",
-        "factsListCount": "列表",
-        "factsExampleCount": "例句",
-        "faqTitle": "常见问题",
+    home: {
+        heroTitle: "查找并学习中文词",
+        searchPlaceholder: "通过拼音或汉字搜索中文词",
+        wordListTitle: "中文词分类表",
+        factsTitle: "网站收录数量",
+        factsWordCount: "词",
+        factsListCount: "列表",
+        factsExampleCount: "例句",
+        faqTitle: "常见问题",
     },
-    "lists": {
-        "title": "中文词分类表",
-        "description": "收集不同场景、不同用途下的中文单词，整理成列表，方便针对性学习",
+    lists: {
+        title: "中文词分类表",
+        description: "收集不同场景、不同用途下的中文单词，整理成列表，方便针对性学习",
+    },
+    word: {
+        information: "基本信息",
+        explanation: "解释",
+        writing: "写法",
+        relatedList: "相关列表",
+        homophones: "同音词",
+
     }
 }
 
