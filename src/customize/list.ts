a7fdbb214723c269404cedfc7cb70a5cfd4f813a
luosen <heyrosen@163.com>
2024-07-07 16:06:35 +0800

    隐藏空列表

--- src/customize/list.ts
+++ src/customize/list.ts
@@ -1,4 +1,8 @@
-import { type IntlListDetail, type Language, type ListDescription } from "@/common/types"
+import {
+    type IntlListDetail,
+    type Language,
+    type ListDescription,
+} from "@/common/types";
 import { chinese, english } from "./lang";
 
 export const hsk1List: IntlListDetail = {
@@ -13,221 +17,228 @@ export const hsk1List: IntlListDetail = {
     english: {
         title: "HSK 1",
         description: "Hanyu Shuiping Kaoshi level-1 words",
-    }
-}
+    },
+};
 
 export const hsk2List: IntlListDetail = {
     name: "hsk2",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk2",
     tags: ["hsk2"],
-    "chinese": {
+    chinese: {
         title: "HSK 2级",
         description: "汉语水平考试1级中包含的词汇",
     },
-    "english": {
+    english: {
         title: "HSK 2",
         description: "Hanyu Shuiping Kaoshi level-2 words",
-    }
-}
+    },
+};
 
 export const hsk3List: IntlListDetail = {
     name: "hsk3",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk3",
     tags: ["hsk3"],
-    "english": {
+    english: {
         title: "HSK 3",
         description: "Hanyu Shuiping Kaoshi level-3 words",
     },
-    "chinese": {
+    chinese: {
         title: "HSK 3级",
         description: "汉语水平考试3级中包含的词汇",
-    }
-}
+    },
+};
 
 export const hsk4List: IntlListDetail = {
     name: "hsk4",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk4",
     tags: ["hsk4"],
-    "english": {
+    english: {
         title: "HSK 4",
         description: "Hanyu Shuiping Kaoshi level-4 words",
     },
-    "chinese": {
+    chinese: {
         title: "HSK 4级",
         description: "汉语水平考试4级中包含的词汇",
-    }
-}
+    },
+};
 
 export const hsk5List: IntlListDetail = {
     name: "hsk5",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk5",
     tags: ["hsk5"],
-    "english": {
+    english: {
         title: "HSK 5",
         description: "Hanyu Shuiping Kaoshi level-5 words",
     },
-    "chinese": {
+    chinese: {
         title: "HSK 5级",
         description: "汉语水平考试5级中包含的词汇",
-    }
-}
+    },
+};
 
 export const hsk6List: IntlListDetail = {
     name: "hsk6",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk6",
     tags: ["hsk6"],
-    "english": {
+    english: {
         title: "HSK 6",
         description: "Hanyu Shuiping Kaoshi level-6 words",
     },
-    "chinese": {
+    chinese: {
         title: "HSK 6级",
         description: "汉语水平考试6级中包含的词汇",
-    }
-}
+    },
+};
 
 export const hskHighList: IntlListDetail = {
     name: "hsk-high",
     imgPath: "/src/assets/images/hsk.jpg",
     url: "/lists/hsk-high",
     tags: ["hsk-high"],
-    "english": {
+    english: {
         title: "HSK High",
         description: "Hanyu Shuiping Kaoshi high level words",
     },
-    "chinese": {
+    chinese: {
         title: "HSK 高级",
         description: "汉语水平考试高级中包含的词汇",
-    }
-}
+    },
+};
 
 export const foodList: IntlListDetail = {
     name: "food",
     imgPath: "/src/assets/images/food.jpg",
     url: "/lists/food",
     tags: ["food", "drink"],
-    "english": {
+    english: {
         title: "Food",
-        description: "Chinese words for food and drink, including fruits, vegetables, meats, etc. and dishes made from these foods",
+        description:
+            "Chinese words for food and drink, including fruits, vegetables, meats, etc. and dishes made from these foods",
     },
-    "chinese": {
+    chinese: {
         title: "食物",
-        description: "汉语中食物相关词汇，包括水果、蔬菜、肉类等，及使用这些食物做成的料理",
+        description:
+            "汉语中食物相关词汇，包括水果、蔬菜、肉类等，及使用这些食物做成的料理",
     },
-}
+};
 
 export const animalList: IntlListDetail = {
     name: "animals",
     imgPath: "/src/assets/images/animal.jpg",
     url: "/lists/animals",
     tags: ["animal"],
-    "english": {
+    english: {
         title: "Animals",
         description: "Chinese words for animals",
     },
-    "chinese": {
+    chinese: {
         title: "动物",
         description: "汉语中动物相关词汇",
     },
-}
+};
 
 export const numberList: IntlListDetail = {
     name: "numbers",
     imgPath: "/src/assets/images/number.jpg",
     url: "/lists/numbers",
     tags: ["number"],
-    "english": {
+    english: {
         title: "Numbers",
-        description: "Chinese words for numbers, including basic numbers, ordinal numbers, fractions, percentages, prices, etc.",
+        description:
+            "Chinese words for numbers, including basic numbers, ordinal numbers, fractions, percentages, prices, etc.",
     },
-    "chinese": {
+    chinese: {
         title: "数字",
-        description: "汉语中数字相关词汇, 包括基本数字、序数、分数、百分数、价格等",
+        description:
+            "汉语中数字相关词汇, 包括基本数字、序数、分数、百分数、价格等",
     },
-}
+};
 
 export const colorList: IntlListDetail = {
     name: "colors",
     imgPath: "/src/assets/images/color.png",
     url: "/lists/colors",
     tags: ["color"],
-    "english": {
+    english: {
         title: "Colors",
         description: "Chinese words for colors",
     },
-    "chinese": {
+    chinese: {
         title: "颜色",
         description: "汉语中颜色相关词汇",
     },
-}
+};
 
 export const businessList: IntlListDetail = {
     name: "business",
     imgPath: "/src/assets/images/greet.jpg",
     url: "/lists/business",
     tags: ["business"],
-    "english": {
+    english: {
         title: "Business",
-        description: "Chinese words for business, including polite expressions, written expressions etc.",
+        description:
+            "Chinese words for business, including polite expressions, written expressions etc.",
     },
-    "chinese": {
+    chinese: {
         title: "商务",
         description: "汉语中商务相关词汇, 包括礼貌用、书面表达等",
     },
-}
+};
 
 export const travelList: IntlListDetail = {
     name: "travel",
     imgPath: "/src/assets/images/travel.webp",
     url: "/lists/travel",
     tags: ["travel", "greet"],
-    "english": {
+    english: {
         title: "Travel",
-        description: "Chinese words for travel, including polite expressions, asking for prices, asking for directions etc.",
+        description:
+            "Chinese words for travel, including polite expressions, asking for prices, asking for directions etc.",
     },
-    "chinese": {
+    chinese: {
         title: "旅行",
         description: "汉语中旅行相关词汇, 包括礼貌用、询问价格、询问路线等",
-    }
-}
+    },
+};
 
 export const dailyList: IntlListDetail = {
     name: "daily",
     imgPath: "/src/assets/images/life.jpeg",
     url: "/lists/daily",
     tags: ["daily"],
-    "english": {
+    english: {
         title: "Daily",
-        description: "Chinese words for daily life, including greetings, shopping etc.",
+        description:
+            "Chinese words for daily life, including greetings, shopping etc.",
     },
-    "chinese": {
+    chinese: {
         title: "日常生活",
         description: "汉语中日常生活相关词汇, 包括问候、购物等",
     },
-}
+};
 
 export const loveList: IntlListDetail = {
     name: "love",
     imgPath: "/src/assets/images/love.jpg",
     url: "/lists/love",
     tags: ["love"],
-    "english": {
+    english: {
         title: "Love",
         description: "Chinese words for love",
     },
-    "chinese": {
+    chinese: {
         title: "爱",
         description: "汉语中表达爱相关的词汇",
     },
-}
+};
 
 export const homeList: IntlListDetail[] = [
-    hsk3List,
+    hsk1List,
     foodList,
     animalList,
     numberList,
@@ -236,16 +247,16 @@ export const homeList: IntlListDetail[] = [
     travelList,
     dailyList,
     loveList,
-]
+];
 
 export const allList: IntlListDetail[] = [
     hsk1List,
-    hsk2List,
-    hsk3List,
-    hsk4List,
-    hsk5List,
-    hsk6List,
-    hskHighList,
+    // hsk2List,
+    // hsk3List,
+    // hsk4List,
+    // hsk5List,
+    // hsk6List,
+    // hskHighList,
     foodList,
     animalList,
     numberList,
@@ -254,9 +265,12 @@ export const allList: IntlListDetail[] = [
     travelList,
     dailyList,
     loveList,
-]
+];
 
-export function getListIntl(intlList: IntlListDetail, lang: Language): ListDescription {
+export function getListIntl(
+    intlList: IntlListDetail,
+    lang: Language,
+): ListDescription {
     switch (lang) {
         case chinese:
             return intlList.chinese;
